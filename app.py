5dfe286db1afdf05f2e9567dd08b0b3715e01acb
fahimhexas2008-hub <fahimhexas2008@gmail.com>
2026-09-18 00:25:00 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-
 from flask import Flask
+import random
 app = Flask(__name__)
-
 @app.route('/')
 def home():
-    return "Signal Bot is Live!"
-
-if __name__ == '__main__':
+    s=random.choice(["BUY","SELL"])
+    p=random.choice(["EURUSD","GBPUSD","USDJPY"])
+    c="lime" if s=="BUY" else "red"
+    return f"<body style='background:black;color:white;text-align:center;padding-top:80px'><h1>QUOTEX BOT LIVE</h1><h2>{p}</h2><h1 style='color:{c};font-size:60px'>{s}</h1><button onclick='location.reload()'>Next</button></body>"
+if __name__=='__main__':
     app.run()
